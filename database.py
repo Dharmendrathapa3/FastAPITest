33a47e6ff0a8a5e879ad0d39a1f219315601d0b9
Dharmendrathapa3 <dharmendrathapa3@gmail.com>
2025-09-17 13:50:15 +0545

    Update Database

--- database.py
+++ database.py
@@ -1,17 +1,18 @@
 import mysql.connector
 from fastapi import HTTPException
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 
 # MySQL connection config - update with your details
 db_config = {
-    'host': 'localhost',
-    'database': 'My_Portfolio',
-    'user': 'root',
-    'password': 'Surkhet@123'
+    'host': os.getenv("DB_HOST"),
+    'database': os.getenv("DB_NAME"),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
 }
-
 # Helper function for DB connection
 def get_connection(dic=False):
     try:
